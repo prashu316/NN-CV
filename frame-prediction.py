23f22d191807660dedabf490fcd9b636d5ca3981
prashu316 <Rasenganrox1@>
2022-01-29 22:14:14 +0530

    code layout to be used

---
+++ frame-prediction.py
@@ -0,0 +1,132 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import io
+import imageio
+from IPython.display import Image, display
+from ipywidgets import widgets, Layout, HBox
+
+# Download and load the dataset.
+fpath = keras.utils.get_file(
+    "moving_mnist.npy",
+    "http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy",
+)
+dataset = np.load(fpath)
+
+# Swap the axes representing the number of frames and number of data samples.
+dataset = np.swapaxes(dataset, 0, 1)
+# We'll pick out 1000 of the 10000 total examples and use those.
+dataset = dataset[:1000, ...]
+# Add a channel dimension since the images are grayscale.
+dataset = np.expand_dims(dataset, axis=-1)
+
+# Split into train and validation sets using indexing to optimize memory.
+indexes = np.arange(dataset.shape[0])
+np.random.shuffle(indexes)
+train_index = indexes[: int(0.9 * dataset.shape[0])]
+val_index = indexes[int(0.9 * dataset.shape[0]) :]
+train_dataset = dataset[train_index]
+val_dataset = dataset[val_index]
+
+# Normalize the data to the 0-1 range.
+train_dataset = train_dataset / 255
+val_dataset = val_dataset / 255
+
+# We'll define a helper function to shift the frames, where
+# `x` is frames 0 to n - 1, and `y` is frames 1 to n.
+def create_shifted_frames(data):
+    x = data[:, 0 : data.shape[1] - 1, :, :]
+    y = data[:, 1 : data.shape[1], :, :]
+    return x, y
+
+
+# Apply the processing function to the datasets.
+x_train, y_train = create_shifted_frames(train_dataset)
+x_val, y_val = create_shifted_frames(val_dataset)
+
+# Inspect the dataset.
+print("Training Dataset Shapes: " + str(x_train.shape) + ", " + str(y_train.shape))
+print("Validation Dataset Shapes: " + str(x_val.shape) + ", " + str(y_val.shape))
+
+#visualising the data
+
+# Construct a figure on which we will visualize the images.
+fig, axes = plt.subplots(4, 5, figsize=(10, 8))
+
+# Plot each of the sequential images for one random data example.
+data_choice = np.random.choice(range(len(train_dataset)), size=1)[0]
+for idx, ax in enumerate(axes.flat):
+    ax.imshow(np.squeeze(train_dataset[data_choice][idx]), cmap="gray")
+    ax.set_title(f"Frame {idx + 1}")
+    ax.axis("off")
+
+# Print information and display the figure.
+print(f"Displaying frames for example {data_choice}.")
+plt.show()
+
+#creating the model
+
+# Construct the input layer with no definite frame size.
+inp = layers.Input(shape=(None, *x_train.shape[2:]))
+
+# We will construct 3 `ConvLSTM2D` layers with batch normalization,
+# followed by a `Conv3D` layer for the spatio-temporal outputs.
+x = layers.ConvLSTM2D(
+    filters=64,
+    kernel_size=(5, 5),
+    padding="same",
+    return_sequences=True,
+    activation="relu",
+)(inp)
+x = layers.BatchNormalization()(x)
+x = layers.ConvLSTM2D(
+    filters=64,
+    kernel_size=(3, 3),
+    padding="same",
+    return_sequences=True,
+    activation="relu",
+)(x)
+x = layers.BatchNormalization()(x)
+x = layers.ConvLSTM2D(
+    filters=64,
+    kernel_size=(1, 1),
+    padding="same",
+    return_sequences=True,
+    activation="relu",
+)(x)
+x = layers.Conv3D(
+    filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same"
+)(x)
+
+# Next, we will build the complete model and compile it.
+model = keras.models.Model(inp, x)
+model.compile(
+    loss=keras.losses.binary_crossentropy, optimizer=keras.optimizers.Adam(),
+)
+
+# Define some callbacks to improve training.
+early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
+reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=5)
+
+# Define modifiable training hyperparameters.
+epochs = 20
+batch_size = 5
+
+# Fit the model to the training data.
+model.summary()
+
+model.fit(
+    x_train,
+    y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    validation_data=(x_val, y_val),
+    callbacks=[early_stopping, reduce_lr],
+)
+
+model.save("trial1.h5")
+
