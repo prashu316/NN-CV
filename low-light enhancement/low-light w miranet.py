0654595db0895c1a933d55809472ed7c01492349
prashu316 <Rasenganrox1@>
2022-02-01 14:36:28 +0530

    functions with model layers

--- low-light enhancement/low-light w miranet.py
+++ low-light enhancement/low-light w miranet.py
@@ -71,3 +71,69 @@ val_dataset = get_dataset(val_low_light_images, val_enhanced_images)
 
 print("Train Dataset:", train_dataset)
 print("Val Dataset:", val_dataset)
+
+#MIRNet Model
+
+def selective_kernel_feature_fusion(
+    multi_scale_feature_1, multi_scale_feature_2, multi_scale_feature_3
+):
+    channels = list(multi_scale_feature_1.shape)[-1]
+    combined_feature = layers.Add()(
+        [multi_scale_feature_1, multi_scale_feature_2, multi_scale_feature_3]
+    )
+    gap = layers.GlobalAveragePooling2D()(combined_feature)
+    channel_wise_statistics = tf.reshape(gap, shape=(-1, 1, 1, channels))
+    compact_feature_representation = layers.Conv2D(
+        filters=channels // 8, kernel_size=(1, 1), activation="relu"
+    )(channel_wise_statistics)
+    feature_descriptor_1 = layers.Conv2D(
+        channels, kernel_size=(1, 1), activation="softmax"
+    )(compact_feature_representation)
+    feature_descriptor_2 = layers.Conv2D(
+        channels, kernel_size=(1, 1), activation="softmax"
+    )(compact_feature_representation)
+    feature_descriptor_3 = layers.Conv2D(
+        channels, kernel_size=(1, 1), activation="softmax"
+    )(compact_feature_representation)
+    feature_1 = multi_scale_feature_1 * feature_descriptor_1
+    feature_2 = multi_scale_feature_2 * feature_descriptor_2
+    feature_3 = multi_scale_feature_3 * feature_descriptor_3
+    aggregated_feature = layers.Add()([feature_1, feature_2, feature_3])
+    return aggregated_feature
+
+def spatial_attention_block(input_tensor):
+    average_pooling = tf.reduce_max(input_tensor, axis=-1)
+    average_pooling = tf.expand_dims(average_pooling, axis=-1)
+    max_pooling = tf.reduce_mean(input_tensor, axis=-1)
+    max_pooling = tf.expand_dims(max_pooling, axis=-1)
+    concatenated = layers.Concatenate(axis=-1)([average_pooling, max_pooling])
+    feature_map = layers.Conv2D(1, kernel_size=(1, 1))(concatenated)
+    feature_map = tf.nn.sigmoid(feature_map)
+    return input_tensor * feature_map
+
+def channel_attention_block(input_tensor):
+    channels = list(input_tensor.shape)[-1]
+    average_pooling = layers.GlobalAveragePooling2D()(input_tensor)
+    feature_descriptor = tf.reshape(average_pooling, shape=(-1, 1, 1, channels))
+    feature_activations = layers.Conv2D(
+        filters=channels // 8, kernel_size=(1, 1), activation="relu"
+    )(feature_descriptor)
+    feature_activations = layers.Conv2D(
+        filters=channels, kernel_size=(1, 1), activation="sigmoid"
+    )(feature_activations)
+    return input_tensor * feature_activations
+
+def dual_attention_unit_block(input_tensor):
+    channels = list(input_tensor.shape)[-1]
+    feature_map = layers.Conv2D(
+        channels, kernel_size=(3, 3), padding="same", activation="relu"
+    )(input_tensor)
+    feature_map = layers.Conv2D(channels, kernel_size=(3, 3), padding="same")(
+        feature_map
+    )
+    channel_attention = channel_attention_block(feature_map)
+    spatial_attention = spatial_attention_block(feature_map)
+    concatenation = layers.Concatenate(axis=-1)([channel_attention, spatial_attention])
+    concatenation = layers.Conv2D(channels, kernel_size=(1, 1))(concatenation)
+    return layers.Add()([input_tensor, concatenation])
+
